fc7cae8112d566d4b5d26058114431397306ded3
JQY <16437320+jqyisbest@users.noreply.github.com>
2024-06-01 18:11:08 +0800

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -57,7 +57,7 @@ mv temp/immortalwrt/package/emortal/default-settings package/emortal/default-set
 bash ../scripts/preset-terminal-tools.sh
 
 # config file
-cp ../config/xiaomi_ax6-stock.config .config
+cp ../config/AX6Modify.config .config
 make defconfig
 
 # # 编译固件
