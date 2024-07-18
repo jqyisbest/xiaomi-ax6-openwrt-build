6bf56ed99aac3dbbe5075e05d891b97a8043a504
JQY <16437320+jqyisbest@users.noreply.github.com>
2024-07-18 09:55:32 +0800

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -69,8 +69,8 @@ mv "luci feed mod/flash.js" feeds/luci/modules/luci-mod-system/htdocs/luci-stati
 # bash ../scripts/preset-terminal-tools.sh
 
 # Modify default IP
-sed -i 's/10.10.10.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # 修改连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
